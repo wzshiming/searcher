c714704c051ab0ed9bbbe9dcf3245a9947050d71
wzshiming <wzshiming@foxmail.com>
2016-04-19 11:36:37 +0800

    搜索多级类别

--- searcher.go
+++ searcher.go
@@ -2,28 +2,40 @@ package searcher
 
 // 搜索类别
 type Searcher struct {
-	categorys map[string]*Keywords
+	categorys map[string]*Searcher
+	keywords  *Keywords
+	parent    *Searcher
 }
 
-func NewSearcher() *Searcher {
+func newSearcher(parent *Searcher) *Searcher {
 	return &Searcher{
-		categorys: map[string]*Keywords{},
+		categorys: map[string]*Searcher{},
+		parent:    parent,
+		keywords:  NewKeywords(),
 	}
 }
 
-func (t *Searcher) Category(category string) *Keywords {
+func NewSearcher() *Searcher {
+	return newSearcher(nil)
+}
+
+func (t *Searcher) Category(category string) *Searcher {
 	c, ok := t.categorys[category]
 	if !ok {
-		c = NewKeywords()
+		c = newSearcher(t)
 		t.categorys[category] = c
 	}
 	return c
 }
 
 func (t *Searcher) Add(key string, p Point, v float64) {
-	t.Category("").Add(key, p, v)
+	t.keywords.Add(key, p, v)
 }
 
 func (t *Searcher) Get(key string) *Values {
-	return t.Category("").Get(key)
+	ret := t.keywords.Get(key)
+	for _, v := range t.categorys {
+		ret = ret.UnionSet(v.Get(key))
+	}
+	return ret
 }
